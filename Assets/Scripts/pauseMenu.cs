626cf013d4f4b71c84c4ae26326a03373eedda5d
Chewah <chewah@users.noreply.github.com>
2020-03-07 16:47:24 -0500

    fixed pause menu

--- Assets/Scripts/pauseMenu.cs
+++ Assets/Scripts/pauseMenu.cs
@@ -20,19 +20,14 @@ public class pauseMenu : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {  
-            paused = !paused;
-
-            if(paused) 
+            if(!paused)
             {
                 Time.timeScale = 0;
                 isVisible = !isVisible;
                 cam.GetComponent<CameraController>().freeze = true;
                 panel.SetActive(isVisible);
+                paused = !paused;
             } else {
-<<<<<<< HEAD
-                
-=======
->>>>>>> 98c8691fe1a91e1eb4a3a8fa2a083319232e7296
                 closeMenu();
             }
             
@@ -40,8 +35,8 @@ public class pauseMenu : MonoBehaviour
     }
 
     public void closeMenu(){
-        Time.timeScale = 1;
         isVisible=!isVisible;
+        paused = !paused;
         panel.SetActive(isVisible);
         Time.timeScale = 1;
         cam.GetComponent<CameraController>().freeze = false;
