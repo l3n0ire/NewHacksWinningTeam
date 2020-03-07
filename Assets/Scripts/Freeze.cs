c392cc045780070f657ce30e676798306a8ac7ae
Chewah <chewah@users.noreply.github.com>
2020-03-07 14:49:33 -0500

    Freeze Script

---
+++ Assets/Scripts/Freeze.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freeze : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject[] obs;
+
+    void Start()
+    {
+        obs = GameObject.FindGameObjectsWithTag("object");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {  
+            foreach (GameObject ob in obs) 
+            {
+                ob.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            }
+        }
+    }
+}
