7ed8a4b570a8ff443034b2d20407b5047557f274
Colin Lin <colincool100@gmail.com>
2020-03-07 13:43:50 -0500

    arrow
    asdf

---
+++ Assets/Scripts/arrow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+    public GameObject gravityArrow;
+    public GameObject myObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gravityArrow.transform.position = myObject.transform.position;
+        gravityArrow.transform.rotation = Quaternion.Euler(0,0,0);
+        
+    }
+}
