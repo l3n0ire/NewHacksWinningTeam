40a7a564a1e6d0d31fdc58d59d7d3b2deaa72779
Colin Lin <colincool100@gmail.com>
2020-03-07 12:07:55 -0500

    slope

---
+++ Assets/Scripts/slope.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class slope : MonoBehaviour
+{
+    int angle;
+    public InputField angleInput;
+    Transform pivot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pivot =pivot = GetComponent<Transform>();
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+        angle=Int32.Parse(angleInput.text.ToString());
+        pivot.rotation = Quaternion.Euler(0,0,-angle);
+        
+    }
+}
