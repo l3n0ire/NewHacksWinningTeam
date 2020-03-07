20bdf7cc30bf6f0196e57e362d1b8969ae25f8bb
Chewah <chewah@users.noreply.github.com>
2020-03-07 12:10:51 -0500

    Camera Motion with Mouse
    wasd are not relative

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,26 +5,37 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
-    private float moveSpeed = 1.0f;
+    private float moveSpeed = 10.0f;
     private float scrollSpeed = 10.0f;
     float horizontalInput;
     float verticalInput;
     float scrollInput;
+
+    float mouseX;
+    float mouseY;
+    private Vector3 velocity = Vector3.zero;
     void Update()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
         scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        mouseX = (Input.mousePosition.x / Screen.width ) - 0.5f;
+        mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+        transform.localRotation = Quaternion.Euler (new Vector4 (-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
     }
 
     void FixedUpdate() 
-    {
-        if (Input.GetAxisRaw("Horizontal") != 0 || verticalInput != 0) {
-            transform.position += moveSpeed * new Vector3(horizontalInput, 0, verticalInput);
+    {   
+        
+        if (horizontalInput != 0 || verticalInput != 0) {
+            Vector3 newPosition = new Vector3(horizontalInput, 0, verticalInput) * moveSpeed + transform.position;
+            transform.position = Vector3.Lerp(transform.position, newPosition, 0.8f);
         }
-
         if (Input.GetAxis("Mouse ScrollWheel") != 0) {
-            transform.position += scrollSpeed * new Vector3(0, -scrollInput, 0);
+            Vector3 newPosition1 = new Vector3(0, -scrollInput, 0) * scrollSpeed + transform.position;
+            transform.position = Vector3.Lerp(transform.position, newPosition1, 0.8f);
+
+            //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         }
     }
 
