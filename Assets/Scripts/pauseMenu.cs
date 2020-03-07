817b595cd3eedf027b0e91f4ff74cdc284b3f29e
Chidubem Ogbudibe <chidubem@live.ca>
2020-03-07 16:50:28 -0500

    update graph

---
+++ Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour
+{
+    public bool isVisible =false;
+    public GameObject panel;
+    bool paused = false;
+    public GameObject cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(isVisible);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {  
+            paused = !paused;
+
+            if(paused) 
+            {
+                Time.timeScale = 0;
+                isVisible = !isVisible;
+                cam.GetComponent<CameraController>().freeze = true;
+                panel.SetActive(isVisible);
+            } else {
+                closeMenu();
+            }
+            
+        }
+    }
+
+    public void closeMenu(){
+        Time.timeScale = 1;
+        isVisible=!isVisible;
+        panel.SetActive(isVisible);
+        cam.GetComponent<CameraController>().freeze = false;
+
+    }
+}
