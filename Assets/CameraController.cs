1bc38a577b71e2dd9b6bc4b88fa58319e563f514
Colin Lin <colincool100@gmail.com>
2020-03-07 17:10:24 -0500

    revert

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
-    float moveSpeed = 10.0f;
-    float scrollSpeed = 10.0f;
+    private float moveSpeed = 10.0f;
+    private float scrollSpeed = 10.0f;
     float horizontalInput;
     float verticalInput;
     float scrollInput;
+    float velocity = 100;
+
     float mouseX;
     float mouseY;
     float speed = 10;
-    public bool freeze = false;
-
     void Update()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -22,35 +22,29 @@ public class CameraController : MonoBehaviour
         scrollInput = Input.GetAxis("Mouse ScrollWheel");
         mouseX = (Input.mousePosition.x / Screen.width ) - 0.5f;
         mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+<<<<<<< HEAD
         if(!freeze)
         {
             transform.localRotation = Quaternion.Euler (new Vector4 (-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
         } 
+=======
+        transform.localRotation = Quaternion.Euler (new Vector4 (-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
+>>>>>>> parent of 817b595... update graph
     }
 
     void FixedUpdate() 
-    {
-        if (horizontalInput < 0) {
-            Vector3 newPosition = new Vector3(horizontalInput, 0, 0) * moveSpeed + transform.position;
-            transform.position = transform.position - Camera.main.transform.right * speed * Time.fixedDeltaTime;//Vector3.Lerp(transform.position, newPosition, 0.8f);
-        }
-        if (horizontalInput > 0) {
-            Vector3 newPosition = new Vector3(horizontalInput, 0, 0) * moveSpeed + transform.position;
-            transform.position = transform.position + Camera.main.transform.right * speed * Time.fixedDeltaTime;//Vector3.Lerp(transform.position, newPosition, 0.8f);
-        }
-        if (verticalInput < 0) {
-            Vector3 newPosition = new Vector3(0, 0, verticalInput) * moveSpeed + transform.position;
-            transform.position = transform.position - Camera.main.transform.forward * speed * Time.fixedDeltaTime;//Vector3.Lerp(transform.position, newPosition, 0.8f);
-        }
-        if (verticalInput > 0) {
-            Vector3 newPosition = new Vector3(0, 0, verticalInput) * moveSpeed + transform.position;
-            transform.position = transform.position + Camera.main.transform.forward * speed * Time.fixedDeltaTime;//Vector3.Lerp(transform.position, newPosition, 0.8f);
+    {   
+        
+        if (horizontalInput != 0 || verticalInput != 0) {
+            Vector3 newPosition = new Vector3(horizontalInput, 0, verticalInput) * moveSpeed + transform.position;
+            transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;//Vector3.Lerp(transform.position, newPosition, 0.8f);
         }
         if (Input.GetAxis("Mouse ScrollWheel") != 0) {
             Vector3 newPosition1 = new Vector3(0, -scrollInput, 0) * scrollSpeed + transform.position;
             transform.position = Vector3.Lerp(transform.position, newPosition1, 0.8f);
+
+            //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         }
-        
     }
 
 }
--- Assets/Scripts/slope.cs
+++ Assets/Scripts/slope.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+
 public class slope : MonoBehaviour
 {
     int angle;
     public InputField angleInput;
     Transform pivot;
-    
 
     // Start is called before the first frame update
     void Start()
     {
-        pivot =GameObject.Find("pivot").GetComponent<Transform>();
-
+        pivot =pivot = GetComponent<Transform>();
         
 
         
@@ -23,9 +22,6 @@ public class slope : MonoBehaviour
     // Update is called once per frame
     void Update()
     {    
-        
-    }
-    public void setSlope(){
         angle=Int32.Parse(angleInput.text.ToString());
         pivot.rotation = Quaternion.Euler(0,0,-angle);
         
