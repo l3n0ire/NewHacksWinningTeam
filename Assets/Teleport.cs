a13b10900091e27ae570d9fed2760b615a32fd0e
Mitravasu Prakash <43656604+Mitravasu@users.noreply.github.com>
2020-03-07 23:17:41 -0500

    Teleport
    Ability to Transform

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class ObjectController : MonoBehaviour
+public class Teleport : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter(Collision other) {
+        SceneManager.LoadScene("Newton");
     }
 }
